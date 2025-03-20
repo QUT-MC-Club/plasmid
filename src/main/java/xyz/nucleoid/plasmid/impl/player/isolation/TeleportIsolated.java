bd95a1301cf43d27cf458814c2d67fee39350919
ZekeZ <contact@zekez.dev>
2025-03-21 03:36:34 +1000

    Revert "Make teleportation isolation stricter"
    This reverts commit e5f3c2f4870c7e54ed1a91e34632ed4bb4ccd48c.

--- src/main/java/xyz/nucleoid/plasmid/impl/player/isolation/TeleportIsolated.java
+++
@@ -1,5 +0,0 @@
-package xyz.nucleoid.plasmid.impl.player.isolation;
-
-public interface TeleportIsolated {
-    void plasmid$setTeleportIsolation(boolean value);
-}
--- src/main/java/xyz/nucleoid/plasmid/mixin/game/space/ServerPlayerEntityMixin.java
+++ src/main/java/xyz/nucleoid/plasmid/mixin/game/space/ServerPlayerEntityMixin.java
@@ -9,32 +9,39 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.TeleportTarget;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import xyz.nucleoid.plasmid.api.game.GameSpaceManager;
 import xyz.nucleoid.plasmid.impl.game.manager.GameSpaceManagerImpl;
-import xyz.nucleoid.plasmid.impl.player.isolation.TeleportIsolated;
 
 @Mixin(ServerPlayerEntity.class)
-public abstract class ServerPlayerEntityMixin extends PlayerEntity implements TeleportIsolated {
-    @Unique
-    private boolean teleportIsolation = true;
-
+public abstract class ServerPlayerEntityMixin extends PlayerEntity {
     private ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
         super(world, pos, yaw, profile);
     }
 
-    @Inject(method = "teleportTo(Lnet/minecraft/world/TeleportTarget;)Lnet/minecraft/server/network/ServerPlayerEntity;", at = @At("HEAD"), cancellable = true)
-    private void preventOutOfGameTeleports(TeleportTarget teleportTarget, CallbackInfoReturnable<Object> cir) {
-        if (this.teleportIsolation && GameSpaceManager.get().byPlayer(this) != GameSpaceManager.get().byWorld(teleportTarget.world())) {
-            cir.setReturnValue(this);
+    @Inject(method = "teleportTo", at = @At("HEAD"), cancellable = true)
+    private void onTeleport(TeleportTarget target, CallbackInfoReturnable<Entity> ci) {
+        if (this.getWorld() != target.world() && !this.tryTeleportTo(target.world())) {
+            ci.setReturnValue(this);
         }
     }
 
-    @Override
-    public void plasmid$setTeleportIsolation(boolean value) {
-        this.teleportIsolation = value;
+    private boolean tryTeleportTo(ServerWorld targetWorld) {
+        var gameSpaceManager = GameSpaceManagerImpl.get();
+        var playerGameSpace = gameSpaceManager.byPlayer(this);
+        var targetGameSpace = gameSpaceManager.byWorld(targetWorld);
+        if (playerGameSpace == targetGameSpace) {
+            return true;
+        }
+
+        if (playerGameSpace != null && targetGameSpace == null) {
+            var self = (ServerPlayerEntity) (Object) this;
+            playerGameSpace.getPlayers().remove(self);
+            playerGameSpace.getPlayers().getTeleporter().teleportOutTo(self, targetWorld);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
